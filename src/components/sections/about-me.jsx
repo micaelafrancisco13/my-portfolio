356994f2f690753fc33055405eb972b867f97891
Ela <micaela.francisco.s@bulsu.edu.ph>
2023-06-21 23:50:20 +0800

    add mroe YT channels

--- src/components/sections/about-me.jsx
+++ src/components/sections/about-me.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Box,
   Fade,
@@ -6,33 +6,33 @@ import {
   Slide,
   Typography,
   useMediaQuery,
-} from '@mui/material'
-import { StyledLine } from '../../styled/sections/projects/projects.styled'
-import theme from '../../theme/main'
-import Avatar from '../../images/yt-channels/avatar'
-import { getYtChannels } from '../../utils/yt-channels'
-import { useInView } from 'react-intersection-observer'
+} from "@mui/material";
+import { StyledLine } from "../../styled/sections/projects/projects.styled";
+import theme from "../../theme/main";
+import Avatar from "../../images/yt-channels/avatar";
+import { getYtChannels } from "../../utils/yt-channels";
+import { useInView } from "react-intersection-observer";
 
 const AboutMe = ({ aboutMeRef }) => {
-  const smallAndLarger = useMediaQuery(theme.breakpoints.up('sm'))
-  const largeAndLarger = useMediaQuery(theme.breakpoints.up('lg'))
-  const ytChannels = [...getYtChannels()]
-  const { ref: channelsRef, inView: areChannelsInView } = useInView()
-  const { ref: digitalArtRef, inView: isDigitalArtInView } = useInView()
+  const smallAndLarger = useMediaQuery(theme.breakpoints.up("sm"));
+  const largeAndLarger = useMediaQuery(theme.breakpoints.up("lg"));
+  const ytChannels = [...getYtChannels()];
+  const { ref: channelsRef, inView: areChannelsInView } = useInView();
+  const { ref: digitalArtRef, inView: isDigitalArtInView } = useInView();
 
   const digitalArtComponent = (
     <img
       className="digital-art-borealis"
-      src={require('../../images/dall-e-image.png')}
+      src={require("../../images/dall-e-image.png")}
       alt="Digital art of a woman sitting on a computer chair with her laptop, outside, overlooking the Auora Borealis above her"
     />
-  )
+  );
 
   return (
     <Box
       // mb={{ xs: "10rem", lg: "14rem", xl: "16rem", xxl: "25rem" }}
-      mb={{ xs: '10rem', lg: '14rem', xl: '18rem', xxl: '22rem' }}
-      sx={{ height: { lg: '100vh' } }}
+      mb={{ xs: "10rem", lg: "14rem", xl: "18rem", xxl: "22rem" }}
+      sx={{ height: { lg: "100vh" } }}
       component="section"
     >
       <Grid container>
@@ -61,18 +61,22 @@ const AboutMe = ({ aboutMeRef }) => {
         <Grid item xs={12} md={7}>
           <Typography>
             Hi, I'm Ela! A 4th year undergraduate IT student at Bulacan State
-            University. I have experience in HTML, CSS, JavaScript, and web
+            University. With my passion for technology and love for learning, I
+            always strive to keep up with the latest trends. Below are some of
+            my favorite tech-related YouTube channels - I enjoy watching them
+            all, they're a must-see!
+            {/* I have experience in HTML, CSS, JavaScript, and web
             development libraries and frameworks such as React and Express.
             Although I am well-versed in web development, I am currently looking
-            to expand my skill set and learn more about mobile development.
+            to expand my skill set and learn more about mobile development. */}
           </Typography>
           <br />
-          <Typography>
+          {/* <Typography>
             With my passion for technology and love for learning, I always
             strive to keep up with the latest trends. Below are some of my
             favorite tech-related YouTube channels - I enjoy watching them all,
             they're a must-see!
-          </Typography>
+          </Typography> */}
           <br />
           <Grid container spacing="2rem" ref={channelsRef}>
             {ytChannels.map((channel, index) => (
@@ -111,10 +115,10 @@ const AboutMe = ({ aboutMeRef }) => {
         </Grid>
       </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
 
 {
   /* <Typography>
--- src/utils/yt-channels.js
+++ src/utils/yt-channels.js
@@ -10,6 +10,16 @@ const ytChannels = [
     alt: "Fireship's YouTube channel",
   },
   {
+    name: "Theo - t3.gg",
+    avatarFileName: `Theo_T3.png`,
+    alt: "Theo - t3.gg's YouTube channel",
+  },
+  {
+    name: "Lex Fridman",
+    avatarFileName: "Lex_Fridman.png",
+    alt: "Lex Fridman's YouTube channel",
+  },
+  {
     name: "Jeremy's IT Lab",
     avatarFileName: `Jeremy's_IT_Lab.png`,
     alt: "Jeremy's IT Lab's YouTube channel",
@@ -20,9 +30,9 @@ const ytChannels = [
     alt: "NetworkChuck's YouTube channel",
   },
   {
-    name: "Lex Fridman",
-    avatarFileName: "Lex_Fridman.png",
-    alt: "Lex Fridman's YouTube channel",
+    name: "Byte Byte Go",
+    avatarFileName: "Byte_Byte_Go.png",
+    alt: "Byte Byte Go's YouTube channel",
   },
   {
     name: "IBM Technology",
