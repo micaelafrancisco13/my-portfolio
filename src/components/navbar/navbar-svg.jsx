918e4bcb7d80a749ea13ad47b939209da5aaa17f
Ela <micaela.francisco.s@bulsu.edu.ph>
2023-06-22 00:05:38 +0800

    change icon

--- src/components/navbar/navbar-svg.jsx
+++ src/components/navbar/navbar-svg.jsx
@@ -19,7 +19,7 @@ const NavbarSVG = () => {
 
   return (
     <Box className="flex-y-center">
-      <svg
+      {/* <svg
         width={width}
         height={height}
         viewBox="0 0 52 52"
@@ -32,7 +32,8 @@ const NavbarSVG = () => {
           d="M39 0H26V13H13H0V26V39V52H13H26V39H39H52V26V13V0H39ZM13 39H26V26H39V13H26V26H13V39Z"
           fill="white"
         />
-      </svg>
+      </svg> */}
+      💛
     </Box>
   );
 };
